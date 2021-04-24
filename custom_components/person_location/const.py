15683ae825d0de866a853ed84fc4630dadbec1b4
Rod Payne <rodpayne@byu.edu>
2021-04-24 08:35:42 -0600

    Bump version

--- custom_components/person_location/const.py
+++ custom_components/person_location/const.py
@@ -24,7 +24,7 @@ DOMAIN = "person_location"
 API_STATE_OBJECT = DOMAIN + "." + DOMAIN + "_integration"
 INTEGRATION_NAME = "Person Location"
 ISSUE_URL = "https://github.com/rodpayne/home-assistant/issues"
-VERSION = "2021.04.10"
+VERSION = "2021.04.24"
 
 # Fixed parameters:
 MIN_DISTANCE_TRAVELLED_TO_GEOCODE = 5
