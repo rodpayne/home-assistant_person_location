7de312b410b86eebfd28c98c704ae2e419e40c3d
RodPayne <roderickhpayne@gmail.com>
2024-06-08 08:39:56 -0600

    Bump version to valid name. #17

--- custom_components/person_location/const.py
+++ custom_components/person_location/const.py
@@ -35,7 +35,7 @@ DOMAIN = "person_location"
 API_STATE_OBJECT = DOMAIN + "." + DOMAIN + "_integration"
 INTEGRATION_NAME = "Person Location"
 ISSUE_URL = "https://github.com/rodpayne/home-assistant_person_location/issues"
-VERSION = "2024.06.07b"
+VERSION = "2024.06.08"
 
 # Constants:
 METERS_PER_KM = 1000
